45fd98205536ca6100fe181422ab85b77e9c4663
Desko Sekulović <desko.sekulovic@gmail.com>
2020-04-27 13:20:48 +0200

    navigation styling and app module refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,17 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
 import { HttpClientModule } from "@angular/common/http";
+import { ChartsModule } from "ng2-charts";
+import { AgmCoreModule } from "@agm/core";
 
 import { AppRoutingModule } from "./app-routing.module";
+import { GOOGLE_API_KEY } from "./utility/constants";
+
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { LocationComponent } from "./location/location.component";
-
-import { ChartsModule } from "ng2-charts";
-import { AgmCoreModule } from "@agm/core";
-
-/*
-weather api:
-http://api.openweathermap.org/data/2.5/weather?lat=lat&lon=lon&appid=c3b8f2e28ea9bf6f5d8bcb678001ab74
-*/
-
-const appRoutes: Routes = [
-  { path: "", component: HomeComponent },
-  { path: "location", component: LocationComponent },
-  { path: "about", component: AboutComponent },
-  { path: "**", redirectTo: "" },
-];
+import { InfoCardComponent } from "./info-card/info-card.component";
 
 @NgModule({
   declarations: [
@@ -30,15 +19,15 @@ const appRoutes: Routes = [
     HomeComponent,
     AboutComponent,
     LocationComponent,
+    InfoCardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes),
     HttpClientModule,
     ChartsModule,
     AgmCoreModule.forRoot({
-      apiKey: "AIzaSyCOAZ3sQ2kjkL8f7Uoil6B3R4fT562HsXM",
+      apiKey: GOOGLE_API_KEY,
     }),
   ],
   providers: [],
---
+++ src/app/utility/constants.ts
@@ -0,0 +1,2 @@
+export const URL_SUMMARY = "https://api.covid19api.com/summary";
+export const GOOGLE_API_KEY = "AIzaSyCOAZ3sQ2kjkL8f7Uoil6B3R4fT562HsXM";
