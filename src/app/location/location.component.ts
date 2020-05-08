8def3592365e6da0ab478059ccff5407bef50f6b
Desko Sekulović <desko.sekulovic@gmail.com>
2020-05-08 14:11:26 +0200

    added table and total numbers for selected country

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -27,6 +27,7 @@ export class LocationComponent implements OnInit, OnDestroy {
   searchText: string;
   subs: Subscription[] = [];
   activeCountry: string;
+  totalData: {};
 
   text: string;
   activeSort: number;
@@ -83,6 +84,12 @@ export class LocationComponent implements OnInit, OnDestroy {
 
           if (data.length) {
             this.loadChartData(0);
+            let lastElement = this.mainData[this.mainData.length - 1];
+            this.totalData = {
+              TotalConfirmed: lastElement.Confirmed,
+              TotalRecovered: lastElement.Recovered,
+              TotalDeaths: lastElement.Deaths,
+            };
 
             this.latitude = +data[0].Lat;
             this.longitude = +data[0].Lon;
